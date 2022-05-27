d09d18b7858720e3cf22c4e17d9048bb4f23772a
Mitchell George <mitchgeorge8@gmail.com>
2022-05-27 19:15:22 -0400

    Added inquirer tree and updated "all" queries

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,41 +1,26 @@
 INSERT INTO department (name)
 VALUES
-  ("Production"),
-  ("Research & Development"),
   ("Sales"),
-  ("Marketing"),
-  ("Human Resources"),
-  ("Accounting");
+  ("Engineering"),
+  ("Marketing");
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-  ("Production Manager", 100000, 1),
-  ("Research & Development Manager", 100000, 2),
-  ("Sales Manager", 100000, 3),
-  ("Marketing Manager", 100000, 4),
-  ("Human Resources Manager", 100000, 5),
-  ("Accounting Manager", 100000, 6),
-  ("Production Engineer", 60000, 1),
-  ("Design Engineer", 60000, 2),
-  ("Sales Representative", 60000, 3),
-  ("Content Producer", 60000, 4),
-  ("Health and Safety Supervisor", 60000, 5),
-  ("Financial Officer", 60000, 6);
+  ("Account Manager", 120000, 1),
+  ("Sales Representative", 80000, 1),
+  ("Lead Engineer", 120000, 2),
+  ("Software Engineer", 80000, 2),
+  ("Marketing Director", 120000, 3),
+  ("Content Specialist", 80000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
   ('James', 'Fraser', 1, NULL),
-  ('Jack', 'London', 2, NULL),
-  ('Robert', 'Bruce', 3, NULL),
-  ('Peter', 'Greenaway', 4, NULL),
-  ('Derek', 'Jarman', 5, NULL),
-  ('Paolo', 'Pasolini', 6, NULL),
-  ('Heathcote', 'Williams', 7, 1),
-  ('Sandy', 'Powell', 7, 1),
-  ('Emil', 'Zola', 8, 2),
-  ('Sissy', 'Coalpits', 8, 2),
-  ('Antoinette', 'Capet', 9, 3),
-  ('Samuel', 'Delany', 9, 3),
-  ('Tony', 'Duvert', 10, 4),
-  ('Dennis', 'Cooper', 11, 5),
-  ('Monica', 'Bellucci', 12, 6);
+  ('Jack', 'London', 2, 1),
+  ('Robert', 'Bruce', 2, 1),
+  ('Peter', 'Greenaway', 3, NULL),
+  ('Derek', 'Jarman', 4, 4),
+  ('Paolo', 'Pasolini', 4, 4),
+  ('Heathcote', 'Williams', 5, NULL),
+  ('Sandy', 'Powell', 6, 7),
+  ('Emil', 'Zola', 6, 7);
--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const main = require("./lib/main");
 // Start server after DB connection
 db.connect((err) => {
   if (err) throw err;
-  console.log("You are now connected to the Employee Tracker.\n");
+  console.log("Now connected to Employee Tracker.\n");
   main();
 });
